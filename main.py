32713d5241c8f2d25a44ae0f89684a2960a6162c
Mayssem Chouaibi <93167448+maycem-chouaibi@users.noreply.github.com>
2023-01-07 20:40:14 +0100

    Revert "preprocessing"

--- main.py
+++ main.py
@@ -2,11 +2,3 @@
 # Description: churn prediction for banking customers
 # Dataset available on Kaggle
 
-import dataHelper
-
-data = dataHelper.load('churn.csv')
-
-data2 = dataHelper.prepareData(data)
-
-x = data2.drop('Exited', axis='columns')
-y = data2['Exited']
